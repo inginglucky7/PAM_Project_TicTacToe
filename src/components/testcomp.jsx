c95213b5b655dc23e2593e58750c75e6a20f7d91
IngCr1mson <61595501+inginglucky7@users.noreply.github.com>
2023-02-25 05:36:03 +0700

    Firebase set up complete

--- src/components/testcomp.jsx
+++ src/components/testcomp.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const testcomp = () => {
     return (
-        <h1 className = "text-amber-500">Hello, World</h1>
+        <div>
+            <h1 className = "text-amber-500">Hello, World</h1>
+            <p>Just a plain text here</p>
+            <p>We were together</p>
+            <p>This is testing subject for real time update</p>
+        </div>
     )
 }
 
